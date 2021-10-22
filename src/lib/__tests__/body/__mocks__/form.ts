c53354f550bdc7024f292b75fe34e57e354d5330
Pranav <pranav@jsbot.io>
2021-10-22 14:34:31 +0530

    form data test cases added

---
+++ src/lib/__tests__/body/__mocks__/form.ts
@@ -0,0 +1,76 @@
+export default {
+    url: {
+      slashes: true,
+      protocol: 'http:',
+      hash: '',
+      pathname: '/data',
+      auth: '',
+      host: 'localhost:3000',
+      port: '3000',
+      hostname: 'localhost',
+      password: '',
+      username: '',
+      origin: 'http://localhost:3000',
+      href: 'http://localhost:3000/data',
+      query_params: [],
+    },
+    method: 'POST',
+    headers: [
+      {
+        key: 'Content-Type',
+        value: 'multipart/form-data',
+        disable: false,
+        id: 'pc0suzBvATAui0NomXdrs',
+        type: 'text',
+      },
+    ],
+    scripts: { pre: '', post: '', test: '' },
+    config: {
+      ORIGIN: '',
+      USERAGENT: '',
+      TIMEOUT_MS: '',
+      REJECT_UNAUTHORIZED: true,
+      FOLLOWLOCATION: true,
+      MAXREDIRS: -1,
+      HTTP_VERSION: 'V2Tls',
+    },
+    meta: {
+      name: 'Untitled Request',
+      description: '',
+      active_auth: 'no_auth',
+      active_body: 'zIEJoPW5psI2sLWXELamQ',
+      type: 'A',
+      version: '1.0',
+    },
+    bodies: {
+      zIEJoPW5psI2sLWXELamQ: {
+        name: 'Default',
+        meta: {
+          is_default: true,
+          active_body_type: 'multipart/form-data',
+        },
+        body: {
+          'multipart/form-data': {
+            value: [
+              {
+                key: 'param1',
+                value: 'value1',
+                disable: false,
+                id: '3lpfaUyHea9R9psDBB3vp',
+                type: 'text',
+              },
+              {
+                key: 'param2',
+                value: 'value2',
+                disable: false,
+                id: 'M4SFYtHSiwOR-URZv3gI1',
+                type: 'text',
+              },
+            ],
+            headers: []
+          },
+        },
+      },
+    },
+  };
+  
